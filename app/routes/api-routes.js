5a21bb5e8693c0376be19c765d472a0bebfb1a97
wilcook <wil.cook.is@gmail.com>
2016-11-19 13:53:03 -0500

    routes based on id working

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -5,70 +5,102 @@ var User = require("../models/Users.js");
 
 module.exports = function(app) {
 
-        app.get("/org/:org_id?", function(req, res) {
+    app.get("/org/:org_id?", function(req, res) {
 
-                    console.log("Organizations say hello");
-
-
-                        // If the user provides a specific character in the URL...
-                        if (req.params.org_id) {
-
-                            // Then display the JSON for ONLY that character.
-                            // (Note how we're using the ORM here to run our searches)
-                            Organization.findAll({
-                                where: {
-                                    routeName: req.params.org_id
-                                }
-                            }).then(function(result) {
-                                res.json(result);
-                            })
-                        }
-
-                        // Otherwise...
-                        else {
-
-                            // Run a quick test to confirm.
-                            Organization.findAll({})
-                                .then(function(result) {
-                                    res.json(result)
-                                })
-                        }
-                    });
+        console.log("Organizations say hello");
 
 
-                    app.get("/event/:event_id?", function(req, res) {
+        // If the user provides a specific org_id in the URL...
+        if (req.params.org_id) {
 
-                        console.log("Events say hello");
 
-                        // Run a quick test to confirm.
-                        Event.findAll({})
-                            .then(function(result) {
-                                res.json(result)
-                            })
-
-                    });
-
-                    app.get("/task/:task_id?", function(req, res) {
-
-                        console.log("Tasks say hello");
-
-                        // Run a quick test to confirm.
-                        Task.findAll({})
-                            .then(function(result) {
-                                res.json(result)
-                            })
-
-                    });
-
-                    app.get("/user/:user_id?", function(req, res) {
-
-                        console.log("Users say hello");
-
-                        // Run a quick test to confirm.
-                        User.findAll({})
-                            .then(function(result) {
-                                res.json(result)
-                            })
-
-                    });
+            Organization.findAll({
+                where: {
+                    org_id: req.params.org_id
                 }
+            }).then(function(result) {
+                res.json(result);
+            })
+        }
+
+        // Otherwise...
+        else {
+
+            // Run a quick test to confirm.
+            Organization.findAll({})
+                .then(function(result) {
+                    res.json(result)
+                })
+        }
+    });
+
+
+    app.get("/event/:event_id?", function(req, res) {
+
+        console.log("Events say hello");
+
+                        if (req.params.user_id) {
+            Event.findAll({
+                where: {
+                    event_id: req.params.task_id
+                }
+            }).then(function(result) {
+                res.json(result);
+            })
+
+        }else{
+
+        // Run a quick test to confirm.
+        Event.findAll({})
+            .then(function(result) {
+                res.json(result)
+            })
+}
+    });
+
+    app.get("/task/:task_id?", function(req, res) {
+
+        console.log("Tasks say hello");
+
+                if (req.params.user_id) {
+                    Task.findAll({
+                        where: {
+                            task_id: req.params.task_id
+                        }
+                    }).then(function(result) {
+                        res.json(result);
+                    })
+
+                } else {
+
+
+        // Run a quick test to confirm.
+        Task.findAll({})
+            .then(function(result) {
+                res.json(result)
+            })
+}
+    });
+
+    app.get("/user/:user_id?", function(req, res) {
+
+        console.log("Users say hello");
+
+        if (req.params.user_id) {
+            User.findAll({
+                where: {
+                    user_id: req.params.user_id
+                }
+            }).then(function(result) {
+                res.json(result);
+            })
+        } else {
+
+            // Run a quick test to confirm.
+            User.findAll({})
+                .then(function(result) {
+                    res.json(result)
+                })
+        }
+    });
+}
