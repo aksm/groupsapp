94df4b9b433e98d112d842764b3790dcb82dd5e1
wilcook <wil.cook.is@gmail.com>
2016-11-19 13:09:09 -0500

    sequelize / tables update

---
+++ app/models/Tasks.js
@@ -0,0 +1,24 @@
+// This may be confusing but here Sequelize (capital) references the standard library
+var Sequelize = require("sequelize");
+
+// sequelize (lowercase) references my connection to the DB. You could name it something else, but I was just following their convention.
+var sequelize = require("../config/connection.js");
+
+var Task = sequelize.define("Task", {
+    task_id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    event_id: Sequelize.INTEGER,
+    task_name: Sequelize.STRING,
+    volunteers_needed: Sequelize.INTEGER,
+    volunteer_count: Sequelize.STRING
+}, {
+    timestamps: false
+});
+
+// Sync with DB
+Task.sync()
+
+module.exports = Task;
--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,17 +1,74 @@
-var Event = require("../models/Events.js")
+var Event = require("../models/Events.js");
+var Organization = require("../models/Organizations.js");
+var Task = require("../models/Tasks.js");
+var User = require("../models/Users.js");
 
-module.exports = function(app){
+module.exports = function(app) {
 
-    app.get("/", function(req, res){
+        app.get("/org/:org_id?", function(req, res) {
 
-        console.log("Hello");
+                    console.log("Organizations say hello");
 
-        // Run a quick test to confirm.
-        Event.findAll({})
-            .then(function(result) {
-                res.json(result)
-            })
 
-    })
+                        // If the user provides a specific character in the URL...
+                        if (req.params.org_id) {
 
-}
+                            // Then display the JSON for ONLY that character.
+                            // (Note how we're using the ORM here to run our searches)
+                            Organization.findAll({
+                                where: {
+                                    routeName: req.params.org_id
+                                }
+                            }).then(function(result) {
+                                res.json(result);
+                            })
+                        }
+
+                        // Otherwise...
+                        else {
+
+                            // Run a quick test to confirm.
+                            Organization.findAll({})
+                                .then(function(result) {
+                                    res.json(result)
+                                })
+                        }
+                    });
+
+
+                    app.get("/event/:event_id?", function(req, res) {
+
+                        console.log("Events say hello");
+
+                        // Run a quick test to confirm.
+                        Event.findAll({})
+                            .then(function(result) {
+                                res.json(result)
+                            })
+
+                    });
+
+                    app.get("/task/:task_id?", function(req, res) {
+
+                        console.log("Tasks say hello");
+
+                        // Run a quick test to confirm.
+                        Task.findAll({})
+                            .then(function(result) {
+                                res.json(result)
+                            })
+
+                    });
+
+                    app.get("/user/:user_id?", function(req, res) {
+
+                        console.log("Users say hello");
+
+                        // Run a quick test to confirm.
+                        User.findAll({})
+                            .then(function(result) {
+                                res.json(result)
+                            })
+
+                    });
+                }
