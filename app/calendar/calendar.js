d0f2ab3ca51c2ac71ee42ee4a04c40ea291cd857
wiljago <wiljago@ERRTHROWN.home>
2016-12-01 18:57:06 -0500

    jQuery removed from calendar.js

--- app/calendar/calendar.js
+++ app/calendar/calendar.js
@@ -1,43 +1,35 @@
-// var gcal = require('google-calendar');
-// var google_calendar = new gcal.GoogleCalendar(accessToken);
+var gcal = require('google-calendar');
+var google_calendar = new gcal.GoogleCalendar(accessToken);
 
-// var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-// var passport = require('passport');
-// var gcal     = require('google-calendar');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var passport = require('passport');
+var gcal     = require('google-calendar');
  
-// passport.use(new GoogleStrategy({
-//     clientID: config.consumer_key,
-//     clientSecret: config.consumer_secret,
-//     callbackURL: "http://localhost:8080/auth/callback",
-//     scope: ['openid', 'email', 'https://www.googleapis.com/auth/calendar'] 
-//   },
-//   function(accessToken, refreshToken, profile, done) {
+passport.use(new GoogleStrategy({
+    clientID: config.consumer_key,
+    clientSecret: config.consumer_secret,
+    callbackURL: "http://localhost:8080/auth/callback",
+    scope: ['openid', 'email', 'https://www.googleapis.com/auth/calendar'] 
+  },
+  function(accessToken, refreshToken, profile, done) {
     
-//     //google_calendar = new gcal.GoogleCalendar(accessToken); 
+    //google_calendar = new gcal.GoogleCalendar(accessToken); 
     
-//     return done(null, profile);
-//   }
-// ));
+    return done(null, profile);
+  }
+));
 
-// var google_calendar = new gcal.GoogleCalendar(accessToken);
+var google_calendar = new gcal.GoogleCalendar(accessToken);
  
-// google_calendar.calendarList.list(function(err, calendarList) {
+google_calendar.calendarList.list(function(err, calendarList) {
   
-//   ...
+  ...
   
-//   google_calendar.events.list(calendarId, function(err, calendarList) {
+  google_calendar.events.list(calendarId, function(err, calendarList) {
     
-//     ...
-//   });
-// });
-
-// module.exports = google_calendar;
-
-$(document).ready(function() {
-    $('#calendar').fullCalendar({
-        googleCalendarApiKey: 'AIzaSyDmhYw5oa57Me3sN606pv6qaCOk7YCV17Y',
-        events: {
-            googleCalendarId: 'il4maior5s5jn2kbkl5n9ppb8s@group.calendar.google.com'
-        }
-    });
+    ...
+  });
 });
+
+module.exports = google_calendar;
+
