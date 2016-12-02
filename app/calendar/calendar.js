fb7f990f07801957ca809e25b9cd0adbfb2a94fc
aksm <albert.min@gmail.com>
2016-12-01 19:12:54 -0500

    Comment out calendarjs

--- app/calendar/calendar.js
+++ app/calendar/calendar.js
@@ -1,23 +1,23 @@
-var googlecal = require('google-calendar');
-var google_calendar = new gcal.GoogleCalendar(accessToken);
+// var googlecal = require('google-calendar');
+// var google_calendar = new gcal.GoogleCalendar(accessToken);
 
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-var passport = require('passport');
+// var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+// var passport = require('passport');
 
  
-passport.use(new GoogleStrategy({
-    clientID: config.consumer_key,
-    clientSecret: config.consumer_secret,
-    callbackURL: "http://localhost:8080/auth/callback",
-    scope: ['openid', 'email', 'https://www.googleapis.com/auth/calendar'] 
-  },
-  function(accessToken, refreshToken, profile, done) {
+// passport.use(new GoogleStrategy({
+//     clientID: config.consumer_key,
+//     clientSecret: config.consumer_secret,
+//     callbackURL: "http://localhost:8080/auth/callback",
+//     scope: ['openid', 'email', 'https://www.googleapis.com/auth/calendar'] 
+//   },
+//   function(accessToken, refreshToken, profile, done) {
     
     //google_calendar = new gcal.GoogleCalendar(accessToken); 
     
-    return done(null, profile);
-  }
-));
+//     return done(null, profile);
+//   }
+// ));
 
 var google_calendar = new gcal.GoogleCalendar(accessToken);
  
